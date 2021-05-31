b320fc2e4f029c9542212406b8d786bb6766155f
karcher <karcher@embl.de>
2021-05-31 11:12:52 +0200

    fixed process.sh

--- script/process.sh
+++ script/process.sh
@@ -1,51 +1,41 @@
 #!/bin/bash -e
 
-#for sampleID in 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample1_sequence  000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample4_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample5_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample6_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample2_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample3_sequence
-for sampleID in CV001I10_all_forward_reads_concatenated_140000_reads_per_sample
+#for sampleID in 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample1_sequence  000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample4_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample5_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample6_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample2_sequence 000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample3_sequence CV001I10_all_forward_reads_concatenated_140000_reads_per_sample
+
+experimentName="CV001N"
+
+for sampleID in $(ls "../data/experiments/${experimentName}/" | sed "s/\.fastq//")
 do
-#sampleID="HMW5KAFX2_CV001L3_21s001573-1-1_Voogdt_lane1Sample2_sequence"
 
 # Filter reads. TODO: Remove low quality reads?!
 echo "Filtering reads and getting barcodes..."
-python filter_reads_and_get_barcodes.py ${sampleID}.fastq ${sampleID}_filtered.fastq results/${sampleID}_readname_barcode.tab
-#python filter_reads.py ${sampleID}.fastq ${sampleID}_filtered_stringent.fastq
+python filter_reads_and_get_barcodes.py ../data/experiments/${experimentName}/${sampleID}.fastq ../data/experiments/${experimentName}/${sampleID}_filtered.fastq ../data/experiments/${experimentName}/${sampleID}_readname_barcode.tab
 
-
-# Transform reads to fasta
-#paste <(cat ${sampleID}.fastq | paste - - - - | cut -f1) <(cat ${sampleID}.fastq | paste - - - - | cut -f2) | tr "\t" "\n" > data/${sampleID}.fasta
-
-# Map reads to B. uniformis genome
-# OLD
-#bowtie2 -p 6 -x ~/for_carlos_3/seq/atcc_8492_concatenated --trim5 110 -U ${sampleID}_filtered.fastq -S ${sampleID}.bam
-# New (with internal read splitting in filter_reads.py
+echo "mapping reads..."
 source activate bowtie2
 if [[ $sampleID == "000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample2_sequence" ]] || [[ $sampleID == "000000000-JLF44_CV001N_21s002101-1-1_Voogdt_lane1Sample3_sequence"  ]]
 then
-	echo "we need to map aaginst the B. vulgatus genome in this case."
-	bowtie2 -p 6 -x ~/for_carlos_3/seq/GCF_000012825.1_ASM1282v1_genomic_atcc_8482 -U ${sampleID}_filtered.fastq -S ${sampleID}.bam
+	echo "we need to map aginst the B. vulgatus genome in this case."
+	bowtie2 -p 6 -x ../data/genomes/GCF_000154205.1_ASM15420v1_genomic -U ../data/reads/${experimentName}/${sampleID}_filtered.fastq -S ../results/bam/${sampleID}.bam
 else
-	bowtie2 -p 6 -x ~/for_carlos_3/seq/atcc_8492_concatenated -U ${sampleID}_filtered.fastq -S ${sampleID}.bam
+	bowtie2 -p 6 -x ../data/genomes/atcc_8492_concatenated -U ../data/reads/${experimentName}/${sampleID}_filtered.fastq -S ../results/bam/${sampleID}.bam
 fi
 source deactivate # Could also be conda deactivate?
 
 # Sort bam
 source activate samtools
-samtools sort ${sampleID}.bam > ${sampleID}_sorted.bam
+samtools sort ../results/bam/${sampleID}.bam > ../results/bam/${sampleID}_sorted.bam
 
 # Clean up
 rm ${sampleID}.bam
 
 # Get depth along each position. Cap the depth at 1000 for simplicity.
-samtools depth -a -d 1000 ${sampleID}_sorted.bam > results/depth_${sampleID}.tab
+samtools depth -a -d 1000 ../results/bam/${sampleID}_sorted.bam > ../results/out/depth_${sampleID}.tab
 
-# Get the SAM flag, position, CIGAR flag and sequence of all reads.
-# SAM flag is important because reads mapping on the antisense strand have to be 'reversed' in order to find the position of where the transposon actually inserted.
-# Example: The following is the extracted columns for a reverse-mapping read (SAM flag 16):
-# 16	69	51M	CTCCAGAAAGGAGGTGTTCCAGCCGCACCTTCCGGTACGGCTACCTTGTTA
-# You acn see that the read ends in TA, so the actual insertion site is 69 + 51. This isn't necessary for forward reads
-samtools view ${sampleID}_sorted.bam | cut -f 1,2,4,5,6,10 > results/insertionInfoRaw_${sampleID}.tab
+# Get important columns
+samtools view ../results/bam/${sampleID}_sorted.bam | cut -f 1,2,4,5,6,10 > ../results/out/insertionInfoRaw_${sampleID}.tab
 source deactivate
 
 # Get read length directly (parsing in R is quite painfully slow...)
-paste <(cat results/insertionInfoRaw_${sampleID}.tab) <(cat results/insertionInfoRaw_${sampleID}.tab | cut -f5 | sed "s/M$//") > ${sampleID}.tmp; mv ${sampleID}.tmp results/insertionInfoRaw_${sampleID}.tab
+paste <(cat ../results/out/insertionInfoRaw_${sampleID}.tab) <(cat ../results/out/insertionInfoRaw_${sampleID}.tab | cut -f5 | sed "s/M$//") > ${sampleID}.tmp; mv ${sampleID}.tmp ../results/out/insertionInfoRaw_${sampleID}.tab
 done
